26ae026d70428672bcc0afa4578fbdf43cd35e44
infomanavteerth <info.manavteerth@gmail.com>
2026-01-13 11:19:54 +0530

    add

--- app/page.js
+++ app/page.js
@@ -36,6 +36,27 @@ export default async function HomePage() {
       <TestimonialsCarousel/>    
       <CallToActionSection/>    
       <FAQSection/>     */}
+      <HeroSection />
+      <OurPromiseSection />
+      <div className="max-w-7xl mx-auto bg-[#fdfbf7] py-2">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl font-heading font-extrabold text-[#7C4A0E] text-center m-2">
+          Our Top Selling Khakhra
+        </h2>
+        <p className="mt-4 text-[#cc760e] text-base sm:text-lg md:text-xl leading-relaxed text-center m-2">
+          Freshly crafted, lovingly made. Explore our bestsellers.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+      {/* <KhakhraMakingProcess/>    
+      <CraftsmanshipHeritageSection/>    
+      <ShopByOilPreferenceSection/>    
+      <TestimonialsCarousel/>    
+      <CallToActionSection/>    
+      <FAQSection/>     */}
     Annapurna Khakhra Landing Page
     </div>
   );
